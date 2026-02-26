24dbb8d724c20512f9924a3c8d32af57936a89aa
egamovj <egamovjd09@gmail.com>
2026-02-26 20:38:40 +0500

    buglar fix qilindi

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.pfs73hhmn68"
+    "revision": "0.74m5j6vpb9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
